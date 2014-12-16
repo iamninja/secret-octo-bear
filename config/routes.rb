146819482fb20cb57dc0d8da9c47fc5a87befec4
Vagios Vlachos <bagios234@gmail.com>
2014-12-16 11:16:43 +0200

    Add show action to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ MarketPlaceApi::Application.routes.draw do
                   path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # List resources here
+      resources :users, :only => [:show]
     end
   end
 end
